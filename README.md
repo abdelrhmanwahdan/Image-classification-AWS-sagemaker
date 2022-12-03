afc26dc1b8a27c4f77ca3ea25b75d7460b5d8a2a
abdelrhman wahdan <abdelrhmanwahdan129@gmail.com>
2022-12-03 19:07:15 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# Image Classification using AWS SageMaker
+
+In this project I have used the pretrained resnet18 model I have added one layer and I have used it on the cifar10 dataset using aws sagemaker studio to get fast and deployed model.
+## Project Set Up and Installation
+In order to create this project I have cloned this repositiry  https://github.com/udacity/CD0387-deep-learning-topics-within-computer-vision-nlp-project-starter.git and I have used the aws pytorch 1.8, python 3.6 kernel and the aws ml.m5.large instance to help me run all the nessecary code.
+In addition to that I have installed the smdebug library using pip as follows:
+!pip install smdebug
+
+
+
+## Dataset
+I am using the cifar10 dataset and here is a small description of it: 
+The CIFAR-10 dataset consists of 60000 32x32 colour images in 10 classes, with 6000 images per class. There are 50000 training images and 10000 test images.
+
+The dataset is divided into five training batches and one test batch, each with 10000 images. The test batch contains exactly 1000 randomly-selected images from each class. The training batches contain the remaining images in random order, but some training batches may contain more images from one class than another. Between them, the training batches contain exactly 5000 images from each class.
+### Model used
+ResNet18 is a 72-layer architecture with 18 deep layers. The architecture of this network aimed at enabling large amounts of convolutional layers to function efficiently. However, the addition of multiple deep layers to a network often results in a degradation of the output. This is known as the problem of vanishing gradient where neural networks, while getting trained through back propagation, rely on the gradient descent, descending the loss function to find the minimizing weights. Due to the presence of multiple layers, the repeated multiplication results in the gradient becoming smaller and smaller thereby “vanishing” leading to a saturation in the network performance or even degrading the performance.
+
+The primary idea of ResNet is the use of jumping connections that are mostly referred to as shortcut connections or identity connections. These connections primarily function by hopping over one or multiple layers forming shortcuts between these layers. The aim of introducing these shortcut connections was to resolve the predominant issue of vanishing gradient faced by deep networks. These shortcut connections remove the vanishing gradient issue by again using the activations of the previous layer. These identity mappings initially do not do anything much except skip the connections, resulting in the use of previous layer activations. This process of skipping the connection compresses the network; hence, the network learns faster. This compression of the connections is followed by expansion of the layers so that the residual part of the network could also train and explore more feature space. The input size to the network is 224 × 224 × 3, which is predefined. The network is considered to be a DAG network due to its complex layered architecture and because the layers have input from multiple layers and give output to multiple layers. Residual networks and their variants have broadly been implemented for the analysis of medical images [22, 27–29]. Fig. 4.5 shows the layer architecture of ResNet18 CNN model.
+
+## Hyperparameter Tuning
+In this project I have tuned 3 hyperparameters which are:
+{
+    "lr": ContinuousParameter(0.001, 0.1),
+    "batch-size": CategoricalParameter([32, 64, 128, 256, 512]),
+    "epochs": IntegerParameter(2, 4)
+}
+
+
+![training jobs](./images/training_jobs.png)
+
+![graph](./images/graph.png)
+
+
+
+Remember that your README should:
+- Include a screenshot of completed training jobs
+- Logs metrics during the training process
+- Tune at least two hyperparameters
+- Retrieve the best best hyperparameters from all your training jobs
+
+## Debugging and Profiling
+I have used the smdebug library to do the profiling and the debugging.
+all of my debugging and profiling are included in the profiler-report.ipynb and the profiler-report.html files but here is a short summary:
+
+![debugging](./images/debugging.png)
+
+
+## Model Deployment
+I have deployed the model using the deploy function:
+
+`
+predictor=estimator.deploy(initial_instance_count=1, instance_type="ml.m5.large")  
+`
+
+you can use this function to prepare the input for the model:
+
+`
+file = 'data/cifar-10-batches-py/data_batch_1'
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        Data = pickle.load(fo, encoding='bytes')
+    return Data
+
+Data_extracted=unpickle(file)
+data=np.reshape(Data_extracted[b'data'][0], (3, 32, 32))
+`
+
+
+![endpoint](./images/endpoint.png)
---
+++ train_model.py
@@ -0,0 +1,260 @@
+#TODO: Import your dependencies.
+#For instance, below are some dependencies you might need if you are using Pytorch
+import argparse
+import json
+import logging
+import os
+import sys
+
+
+import torch
+import torch.distributed as dist
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.utils.data
+import torch.utils.data.distributed
+from torchvision import datasets, transforms, models
+
+try:
+    import smdebug.pytorch as smd
+    from smdebug.profiler.utils import str2bool
+except:
+    pass
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+def test(model, test_loader, loss_criterion, hook):
+    '''
+    TODO: Complete this function that can take a model and a 
+          testing data loader and will get the test accuray/loss of the model
+          Remember to include any debugging/profiling hooks that you might need
+    '''
+    
+    if (hook==0):
+        pass
+    else:
+        hook.set_mode(smd.modes.EVAL)
+        
+        
+        
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            output = model(data)
+            test_loss += loss_criterion(output, target).item()  # sum up batch loss
+            pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    logger.info(
+        "Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+            test_loss, correct, len(test_loader.dataset), 100.0 * correct / len(test_loader.dataset)
+        )
+    )
+
+def train(args, model, train_loader, test_loader, optimizer, loss_criterion, hook):
+    '''
+    TODO: Complete this function that can take a model and
+          data loaders for training and will get train the model
+          Remember to include any debugging/profiling hooks that you might need
+    '''
+    
+    if (hook==0):
+        pass
+    else:
+        hook.set_mode(smd.modes.TRAIN)
+
+        
+        
+
+    for epoch in range(1, args.epochs + 1):
+        model.train()
+        for batch_idx, (data, target) in enumerate(train_loader, 1):
+            optimizer.zero_grad()
+            output = model(data)
+            loss = loss_criterion(output, target)
+            loss.backward()
+            optimizer.step()
+            if batch_idx % 100 == 0:
+                logger.info(
+                    "Train Epoch: {} [{}/{} ({:.0f}%)] Loss: {:.6f}".format(
+                        epoch,
+                        batch_idx * len(data),
+                        len(train_loader.dataset),
+                        100.0 * batch_idx / len(train_loader),
+                        loss.item(),
+                    )
+                )
+        test(model, test_loader, loss_criterion, hook)
+        
+    return model
+    
+def Net():
+    '''
+    TODO: Complete this function that initializes your model
+          Remember to use a pretrained model
+    '''
+    model = models.resnet18(pretrained=True)
+
+    for param in model.parameters():
+        param.requires_grad = False   
+
+    num_features=model.fc.in_features
+    model.fc = nn.Sequential(
+                   nn.Linear(num_features, 10))
+    return model
+
+def _get_train_data_loader(batch_size, training_dir):
+    logger.info("Get train data loader")
+    
+    
+    transform_train = transforms.Compose(
+        [
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+        ]
+    )
+    
+    
+    
+    dataset = datasets.CIFAR10(
+        training_dir,
+        train=True,
+        transform=transform_train
+    )
+    
+    
+
+    return torch.utils.data.DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=True
+    )
+
+
+def _get_test_data_loader(test_batch_size, training_dir):
+    logger.info("Get test data loader")
+    
+    
+    transform_valid = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+        ]
+    ) 
+    
+    
+    return torch.utils.data.DataLoader(
+        datasets.CIFAR10(
+            training_dir,
+            train=False,
+            transform=transform_valid
+        ),
+        batch_size=test_batch_size,
+        shuffle=True,
+    )
+
+
+def save_model(model, model_dir):
+    #This Function saves the model as model.pth in the Model directory of the Operating system
+    
+    logger.info("Saving the model.")
+    path = os.path.join(model_dir, "model.pth")
+    torch.save(model.cpu().state_dict(), path)
+      
+def model_fn(model_dir):
+    model = net()
+    with open(os.path.join(model_dir, "model.pth"), "rb") as f:
+        model.load_state_dict(torch.load(f))
+    return model
+
+def main(args):
+
+
+    '''
+    TODO: Initialize a model by calling the net function
+    '''
+
+
+    model = Net()
+    
+    try:
+        hook = smd.Hook.create_from_json_file()
+        hook.register_hook(model)
+    except:
+        hook=0
+        
+        
+        
+    train_loader = _get_train_data_loader(args.batch_size, args.data_dir)
+    test_loader = _get_test_data_loader(args.test_batch_size, args.data_dir)
+    '''
+    TODO: Create your loss and optimizer
+    '''
+    loss_criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    
+    '''
+    TODO: Call the train function to start training your model
+    Remember that you will need to set up a way to get training data from S3
+    '''
+    model=train(args, model, train_loader, test_loader, optimizer, loss_criterion, hook)
+    
+    
+    '''
+    TODO: Save the trained model
+    '''
+    save_model(model, args.model_dir)
+
+if __name__=='__main__':
+    parser=argparse.ArgumentParser()
+    '''
+    TODO: Specify all the hyperparameters you need to use to train your model.
+    '''
+    # Data and model checkpoints directories
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        default=64,
+        metavar="N",
+        help="input batch size for training (default: 64)",
+    )
+    parser.add_argument(
+        "--test-batch-size",
+        type=int,
+        default=1000,
+        metavar="N",
+        help="input batch size for testing (default: 1000)",
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=5,
+        metavar="N",
+        help="number of epochs to train (default: 10)",
+    )
+    parser.add_argument(
+        "--lr", type=float, default=0.01, metavar="LR", help="learning rate (default: 0.01)"
+    )
+    parser.add_argument(
+        "--momentum", type=float, default=0.5, metavar="M", help="SGD momentum (default: 0.5)"
+    )
+
+    # Container environment
+    parser.add_argument("--hosts", type=list, default=json.loads(os.environ["SM_HOSTS"]))
+    parser.add_argument("--current-host", type=str, default=os.environ["SM_CURRENT_HOST"])
+    parser.add_argument("--model-dir", type=str, default=os.environ["SM_MODEL_DIR"])
+    parser.add_argument("--data-dir", type=str, default=os.environ["SM_CHANNEL_TRAINING"])
+    parser.add_argument("--num-gpus", type=int, default=os.environ["SM_NUM_GPUS"])
+
+    args=parser.parse_args()
+    
+    
+    main(args)
